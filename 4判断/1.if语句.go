7014ec93cd18b0a762e0df8c47f123b1ce481f48
wangyouzheng <353735348@qq.com>
2026-05-14 15:40:46 +0800

    feat: - 更新判断语句：初始化+判定模式

--- 4判断/1.if语句.go
+++ 4判断/1.if语句.go
@@ -7,6 +7,13 @@ func main() {
 	var age int
 	fmt.Scan(&age)
 
+	//iffun1(age)
+	iffun2(age)
+}
+
+// 标准if
+func iffun1(age int) {
+
 	if age <= 0 {
 		fmt.Println("未出生")
 		return
@@ -20,4 +27,23 @@ func main() {
 		return
 	}
 	fmt.Println("中年")
+}
+
+// 带初始化变量的if，作用域为if中
+func iffun2(age int) {
+
+	if newage := age; newage <= 0 {
+		fmt.Println("未出生")
+		return
+	}
+	if newage := age; newage <= 18 {
+		fmt.Println("未成年")
+		return
+	}
+	if newage := age; newage >= 19 && newage <= 60 {
+		fmt.Println("青年")
+		return
+	}
+	fmt.Println("中年")
+
 }
